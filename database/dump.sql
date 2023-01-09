b4cca677951fac3074f5cafba07b130f93651ce4
Nolan <81415109+Nolan-ramos@users.noreply.github.com>
2023-01-09 15:35:02 +0100

    create table

--- database/dump.sql
+++ database/dump.sql
@@ -1,14 +1,23 @@
 CREATE TABLE IF NOT EXISTS Users (
     userId INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL,
+    usermail VARCHAR(255) NOT NULL,
     password VARCHAR(256) NOT NULL,
     PRIMARY KEY (userId)
 );
 
 CREATE TABLE IF NOT EXISTS Roommates (
-    roomMateId INT NOT NULL AUTO_INCREMENT,
+    roommateId INT NOT NULL AUTO_INCREMENT,
+    userId INT,
+    PRIMARY KEY (roommateId)
 );
 
 CREATE TABLE IF NOT EXISTS Expenses (
-    ExpensesId INT NOT NULL AUTO_INCREMENT,
+    expenseId INT NOT NULL AUTO_INCREMENT,
+    roommateId INT,
+    PRIMARY KEY (expenseId)
 );
+
+ALTER TABLE Roommates ADD CONSTRAINT FOREIGN KEY (userId) REFERENCES Users(userId);
+
+ALTER TABLE Expenses ADD CONSTRAINT FOREIGN KEY (roommateId) REFERENCES Roommates(roommateId);
