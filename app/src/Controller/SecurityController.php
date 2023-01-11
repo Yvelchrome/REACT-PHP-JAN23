3b7e1ceae4b584a6bc9ab3a2a284640c57acb11d
Yvelchrome <yvelchrome@gmail.com>
2023-01-11 14:47:22 +0100

    feat: SecurityController template for register and login query

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Factory\PDOFactory;
 use App\Framework\Entity\BaseController;
 use App\Framework\Route\Route;
+use App\Manager\UserManager;
 use App\Service\JWTHelper;
 use JetBrains\PhpStorm\NoReturn;
 
@@ -24,10 +26,37 @@ class SecurityController extends BaseController
         ]);
     }*/
 
+    #[NoReturn] #[Route("/register", name: "app_register", methods: ["POST"])]
     public function register()
     {
         $apiInput = json_decode(file_get_contents('php://input'), true);
 
-        $username = $apiInput['username'];
+        $name = $apiInput['name'];
+        $surname = $apiInput['surname'];
+        $email = $apiInput['email'];
+        $plainPassword = $apiInput['password'];
+
+        $user = (new User())->setUserName($name)->setUserSurname($surname)->setUserMail($email)->setPassword($plainPassword);
+
+        $manager = new UserManager(new PDOFactory());
+        $manager->addUser($user);
+    }
+
+    #[NoReturn] #[Route("/login", name: "app_login", methods: ["POST"])]
+    public function login()
+    {
+        $apiInput = json_decode(file_get_contents('php://input'), true);
+
+        $email = $apiInput['mail'];
+        $plainPassword = $apiInput['password'];
+        $user = (new User())->setUserMail($email)->setPassword($plainPassword);
+
+        $manager = new UserManager(new PDOFactory());
+        $check = $manager->checkUser($user);
+
+        if (!$check) {
+            $this->render("login.php", ['error' => 'Le mail ou le mot de passe est incorrect'], "invalid mail or pw");
+        }
+        header('Location: /');
     }
 }
