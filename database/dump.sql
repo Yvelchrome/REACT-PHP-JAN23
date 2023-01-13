1ee963045de0f5fd9e5e87d5a3fcff5d2a66c6b7
Yvelchrome <yvelchrome@gmail.com>
2023-01-13 13:34:45 +0100

    refactor: database update

--- database/dump.sql
+++ database/dump.sql
@@ -1,36 +1,42 @@
-CREATE TABLE IF NOT EXISTS User (
-    userId INT NOT NULL AUTO_INCREMENT,
-    userName VARCHAR(256) NOT NULL,
+CREATE TABLE IF NOT EXISTS User
+(
+    userId      INT          NOT NULL AUTO_INCREMENT,
+    userName    VARCHAR(256) NOT NULL,
     userSurname VARCHAR(256) NOT NULL,
-    userMail VARCHAR(256) NOT NULL,
-    password VARCHAR(256) NOT NULL,
-    admin INT(1) NOT NULL DEFAULT 0,
+    userMail    VARCHAR(256) NOT NULL,
+    password    VARCHAR(256) NOT NULL,
+    admin       INT(1)       NOT NULL DEFAULT 0,
     PRIMARY KEY (userId)
 );
 
-CREATE TABLE IF NOT EXISTS Roommates (
-    roommateId INT NOT NULL AUTO_INCREMENT,
-    userId INT,
+CREATE TABLE IF NOT EXISTS Roommates
+(
+    roommateId   INT          NOT NULL AUTO_INCREMENT,
+    userId       INT          NOT NULL,
+    roommateName VARCHAR(256) NOT NULL DEFAULT '',
     PRIMARY KEY (roommateId),
-    FOREIGN KEY (userId) REFERENCES User(userId) ON DELETE CASCADE
+    FOREIGN KEY (userId) REFERENCES User (userId) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS Expenses (
-    expenseId INT NOT NULL AUTO_INCREMENT,
-    roommateId INT,
-    expenseName VARCHAR(256),
-    expenseSum INT(10),
-    alreadyPayed INT(10),
+CREATE TABLE IF NOT EXISTS Expenses
+(
+    expenseId    INT          NOT NULL AUTO_INCREMENT,
+    roommateId   INT          NOT NULL,
+    expenseName  VARCHAR(256) NOT NULL,
+    expenseSum   INT(10)      NOT NULL,
+    alreadyPayed INT(10)      NOT NULL DEFAULT 0,
+    expenseDate  DATE         NOT NULL DEFAULT CURRENT_DATE,
     PRIMARY KEY (expenseId),
-    FOREIGN KEY (roommateId) REFERENCES Roommates(roommateId)
+    FOREIGN KEY (roommateId) REFERENCES Roommates (roommateId)
 );
 
-CREATE TABLE IF NOT EXISTS ExpenseByUser(
-    ExpenseByUserId INT NOT NULL AUTO_INCREMENT,
-    expenseId INT,
-    userId INT,
-    payed INT(10),
+CREATE TABLE IF NOT EXISTS ExpenseByUser
+(
+    ExpenseByUserId INT     NOT NULL AUTO_INCREMENT,
+    expenseId       INT     NOT NULL,
+    userId          INT     NOT NULL,
+    payed           INT(10) NOT NULL DEFAULT 0,
     PRIMARY KEY (ExpenseByUserId),
-    FOREIGN KEY (expenseId) REFERENCES Expenses(expenseId),
-    FOREIGN KEY (userId) REFERENCES User(userId)
+    FOREIGN KEY (expenseId) REFERENCES Expenses (expenseId),
+    FOREIGN KEY (userId) REFERENCES User (userId)
 )
